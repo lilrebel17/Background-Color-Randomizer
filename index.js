5882b82445436fecd8627656e44c5aa53acdd355
Zachary Farmer <72500445+lilrebel17@users.noreply.github.com>
2022-03-21 00:42:18 -0400

    Completed Project
    Completed the project, gave it a bit of coloring and made everything transparent with some cool border effects. Along with a hover and active effect

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
 let HexParameters = [1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f']
-let BackgroundColor = []
+let HexArray = []
+let BackgroundColor = '#FFFFFF'
 let RandomButton = document.querySelector('.randomizer-button')
+
+CreateHexText();
 
 RandomButton.addEventListener('click', function (){
     for(let i=0; i < 6; i++) {
-        HexRandomizer();
+        CreateHexArray();
     }
-    console.log("Your New Color is " + BackgroundColor)
+    console.log("Your New Color is " + HexArray)
+    ChangeBackgroundColor();
 })
 
-function HexRandomizer() {
+function CreateHexArray() {
         let RandNum = Math.floor(Math.random() * HexParameters.length)
         let NewVar = HexParameters[RandNum]
-        BackgroundColor.push(NewVar)
-        BackgroundColor.join()
-        console.log(BackgroundColor)
-        // SetBackgroundColor()
+        HexArray.push(NewVar)
+        RemovePreviousColor();
 }
 
-// function SetBackgroundColor() {
-//     let Background = document.body.style.backgroundColor
-//     Background = '#' + BackgroundColor
-//     RemovePreviousColor();
-// }
+function RemovePreviousColor() {
+    if(HexArray.length > 6) {
+        HexArray.splice(0,6)
+    }
+}
 
-// function RemovePreviousColor() {
-//     if(BackgroundColor.length < 6) {
+function ChangeBackgroundColor() {
+    BackgroundColor = '#' + HexArray.join('');
+    BackgroundColor = BackgroundColor.toUpperCase()
+    document.body.style.backgroundColor = BackgroundColor
+    console.log(BackgroundColor)
+    CreateHexText();
+}
 
-//     }
-// }
+function CreateHexText() {
+    let HexText = document.querySelector('.hex')
+    HexText.innerHTML = BackgroundColor
+}
