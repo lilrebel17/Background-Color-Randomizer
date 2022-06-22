a46026b503da015c66a0b0e389401ff963012eed
Zachary Farmer <72500445+lilrebel17@users.noreply.github.com>
2022-06-22 09:37:09 -0400

    Completion of save list
    Completed the save list. Now you can save colors to local storage and they retrieve without issues.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 let HexParameters = [1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f']
 let HexArray = ['F','F','F','F','F','F']
-let SaveArray = []
 let BackgroundColor = '#FFFFFF'
 
 const SaveList = document.querySelector('#saved-colors')
@@ -9,7 +8,24 @@ const SaveButton = document.querySelector('#save-button')
 const ClearButton = document.querySelector('#clear-button')
 const DrawerButton = document.querySelector('#drawer-button')
 
+const storagekey = 'colors'
+
 CreateHexText();
+
+window.onload = () => {
+    if(localStorage.getItem(storagekey) == null) {
+        console.log('No local storage found..')
+        ListStateChange(0)
+        return
+    }
+    else {
+        let array = JSON.parse(localStorage.getItem(storagekey))
+        ListStateChange(1)
+        for(let i=0; i < array.length; i++) {
+            MoveHexToList(array[i])
+        }
+    }
+}
 
 RandomButton.addEventListener('click', function (){
     for(let i=0; i < 6; i++) {
@@ -20,28 +36,32 @@ RandomButton.addEventListener('click', function (){
 })
 
 SaveButton.addEventListener('click', () => {
-    SaveHex()
+    MoveHexToList()
+    SaveColorToLocalStorage(BackgroundColor)
+    ListStateChange(1)
 })
 
 ClearButton.addEventListener('click', () => {
-    console.log(SaveList.childNodes.length)
-    for(let i=0; i > SaveList.childNodes.length - 1; i++) {
-        let Child = SaveList.childNodes[i]
-        console.log('I Deleted ' + Child)
-        SaveList.removeChild(Child)
+    for(let i=SaveList.childElementCount; i > 0; i--) {
+        let color = SaveList.childNodes[i]
+        SaveList.removeChild(color)
+        localStorage.clear()
     }
+    ListStateChange(0)
 })
 
 DrawerButton.addEventListener('click', () => {
-    if(SaveList.classList.contains('open')) {
-        console.log("Open")
-        SaveList.classList.add('closed')
-        SaveList.classList.remove('open')
+    if(SaveList.childNodes.length == 1) {
+        console.log("Nothing in list")
+        return
+    }
+    else if(SaveList.classList.contains('open')) {
+        console.log("Closing List")
+        ListStateChange(0)
     }
     else if(SaveList.classList.contains('closed')) {
-        console.log("Closed")
-        SaveList.classList.add('open')
-        SaveList.classList.remove('closed')
+        console.log("Opening List")
+        ListStateChange(1)
     }
 })
 
@@ -74,23 +94,61 @@ function CreateHexText() {
 }
 
 
-function SaveHex() {
+function MoveHexToList(loadedcolor) {
     let SaveList = document.querySelector('#saved-colors')
     let color = BackgroundColor 
 
     const newDiv = document.createElement('div')
     const newP = document.createElement('p')
 
+    if(loadedcolor == null) {
+        newP.innerHTML = color
+        newP.backgroundColor = color
+        newDiv.style.backgroundColor = color;
+    }
+    else {
+        newP.innerHTML = loadedcolor
+        newP.backgroundColor = loadedcolor
+        newDiv.style.backgroundColor = loadedcolor;
+    }
+
     newDiv.append(newP)
     newDiv.classList.add('color')
-    newDiv.style.backgroundColor = color;
-    newDiv.style.paddingTop = '15%';
-    newDiv.style.paddingBottom = '15%';
 
     newP.style.margin = '0px'
-    newP.innerHTML = BackgroundColor;
     newP.style.fontSize = '4vw'
-    newP.backgroundColor = color;
 
     SaveList.append(newDiv)
+}
+
+//Send 0 as variable to close 
+//Send 1 as variable to open
+function ListStateChange(state) {
+    if(state == 0) {
+        console.log("Closed..")
+        SaveList.classList.add('closed')
+        SaveList.classList.remove('open')
+        SaveList.style.overflow = 'hidden'
+        SaveList.style.width = '0px'
+    }
+    else if(state == 1) {
+        console.log("Open..")
+        SaveList.classList.add('open')
+        SaveList.classList.remove('closed')
+        SaveList.style.overflow = 'scroll'
+        SaveList.style.width = 'auto'
+    }
+}
+
+function SaveColorToLocalStorage(color) {
+    if(localStorage.getItem('colors') == null) {
+        let array = []
+        array.push(color)
+        localStorage.setItem(storagekey, JSON.stringify(array))
+    }
+    else {
+        let array = JSON.parse(localStorage.getItem(storagekey))
+        array.push(color)
+        localStorage.setItem(storagekey,JSON.stringify(array))
+    }
 }
